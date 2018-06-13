14178a53706873c453f27b416d2a54a7a602e26a
Prashant Shrestha <intern0t@users.noreply.github.com>
2018-06-13 12:58:10 -0400

    Factorials done, will create a checklist soon.

---
+++ Java/Factorials/Factorials.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+
+public class Factorials{
+    static void Print(String toPrint){
+        System.out.println(toPrint);
+    }
+    
+    public static void getFactorials(int testValue){
+		BigInteger A = new BigInteger("1");
+		for(int i = testValue; i > 1; i--){
+            A = A.multiply(BigInteger.valueOf(i));
+		}
+        Print("|\t" + testValue + "\t|\t" + A.toString());
+	}
+
+	public static void main(String[] args){
+        int toTestFrom = 1;
+        int toTestUntil = 50;
+
+        while(toTestFrom <= toTestUntil){
+            getFactorials(toTestFrom);
+            toTestFrom++;
+        }
+	}
+}
---
+++ Java/Factorials/README.md
@@ -0,0 +1,17 @@
+# Factorials
+
+Factorials using `BigInteger` in Java, `int` has a length limit of `2^31-1`.
+
+![Wikipedia](https://wikimedia.org/api/rest_v1/media/math/render/svg/26dd1fcd06593cc552a96cc44f360b94bb791534)
+> Source Wikipedia
+
+```bash
+javac Factorials.java
+```
+
+```bash
+java Factorials
+```
+
+Output
+![Output (i3-gaps)](https://i.imgur.com/WZ8cTNP.png)
